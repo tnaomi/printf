bedf8c760565a4f3232f5802f81a3e5a2ca54a73
Tadala <naomikape@gmail.com>
2023-09-18 10:33:50 -0700

    Print character %

---
+++ printf_own.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * printf_own - print own character %
+ * using ASCII code for %
+ * Return: %, 1
+*/
+
+int printf_own(void)
+{
+	int c = 37;
+
+	_putchar(c);
+	return (1);
+}
+

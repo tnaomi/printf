4b5dd8daf22b7c85be6a7b7a6df074d47c1ffda8
KARENKAMORI <kamorikaren2019@gmail.com>
2023-09-18 14:40:28 -0700

    printf implementation

--- _printf.c
+++ _printf.c
@@ -1,15 +1,18 @@
 #include "main.h"
-
+/**
+ * _printf - printf function
+ * @format : format string
+ * Return : return output char
+ */
 int _printf(const char *format, ...)
 {
 	char ch;
-	int output_char = 0, string_length = 0; /*tracks the number of characters to be printed*/
-	va_list arg_list
+	int output_char = 0, string_length = 0; /*tracks characters length*/
+	va_list arg_list;
 
 	if (format == NULL) /*Terminate the program if the format string is empty*/
 		return (-1);
-	va_start(arg_list, format); /* the last value of va_start should be the first argument of this function*/
-
+	va_start(arg_list, format); /*format string is last function argument*/
 	while (*format) /* iterates through all characters of our format string*/
 	{
 		if (*format != '%')
@@ -23,30 +26,22 @@ int _printf(const char *format, ...)
 			if (*format == '\0')
 				break;
 			if (*format == '%') /* applies when %% is encountered*/
-			{
 				write(1, format, 1);
 				output_char++;
-			}
 			else if (*format == 'c') /* character handling*/
-			{
 				ch = va_arg(arg_list, int);
 				write(1, &ch, 1);
 				output_char++;
-			}
 			else if (*format == 's') /* string handling*/
-			{
 				char *str = va_arg(arg_list, char*);
 
 				while (str[string_length] != '\0') /* string length calculation*/
-				{
-					string_length++; /*increament the length of the string*/
-				}
-				write(1, str, string_length); /* writing the string to the standard library*/
+					string_length++; /*increament the string length*/
+				write(1, str, string_length); /* writing the string to STDOUT*/
 				output_char += string_length;
-			}
 		}
 		format++;
 	}
 	va_end(arg_list);
-	return output_char;
+	return (output_char);
 }
--- main.h
+++ main.h
@@ -1,10 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
-#include <limits.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
 #include <unistd.h>
@@ -13,14 +9,5 @@
 int printf(const char *format, ...);
 int binary(int num)
 int octal(int num)
-
-/*COMPONENT FXNS*/
-int printf_str(va_list str);
-int printf_char(va_list c);
-int printf_own(void);
-
-/*MISC FXNS*/
-int _strlen(char *str);
-int _putchar(char c);
 
 #endif /* MAIN_H */

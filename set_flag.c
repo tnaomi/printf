2f6e6a4e361b78b7dabc7a72e45b278eb2ebadec
KARENKAMORI <kamorikaren2019@gmail.com>
2023-09-19 13:44:58 -0700

    flag characters for non-custom conversion specifiers

---
+++ set_flag.c
@@ -0,0 +1,31 @@
+/**
+ * _flag - looks for the flag function
+ * @str: input format string
+ * @parameters: arguments structure defined in main.h
+ *
+ * Return: valid flag
+ */
+int _flag(char *str, parameter *parameters)
+{
+	int x = 0;
+
+	switch (*str)
+	{
+		case '+':
+			x = parameters->flag_plus = 1;
+			break;
+		case '-':
+			x = parameters->flag_minus = 1;
+			break;
+		case '0':
+			x = parameters->flag_zero = 1;
+			break;
+		case ' ':
+			x = parameters->flag_space = 1;
+			break;
+		case '#':
+			x = parameters->flag_hashtag = 1;
+			break;
+	}
+	return (i);
+}

edf6600015f6545b7a16de0ad516319e76967b4c
Tadala Kapengule <57710314+tnaomi@users.noreply.github.com>
2023-09-19 13:53:08 +0200

    Added break

--- printf.c
+++ printf.c
@@ -1,8 +1,5 @@
 #include "main.h"
 
-int _strlen(char *str);
-int _putchar(char c, unsigned int size);
-int print_string(char *str);
 /**
 * _printf - printf function
 * @format : format string
@@ -13,15 +10,14 @@ int print_string(char *str);
 int _printf(const char *format, ...)
 {
 	char *str, ch;
-	int len = 0; /*string_length = 0; tracks characters length*/
+	unsigned int len = 0; /*string_length = 0; tracks characters length*/
 	va_list arg_list;
 
 	if (format == NULL) /*Terminate the program if the format string is empty*/
 		return (-1);
 	va_start(arg_list, format); /*format string is last function argument*/
-	while (*format && *format != '\0')
+	while (*format != '\0')
 	{
-		_putchar(*format, 1);
 		if (*format == '%')
 		{
 			format++; /*skips the % character*/
@@ -29,22 +25,25 @@ int _printf(const char *format, ...)
 			{
 				_putchar(37, 1);
 				len++;
+				break;
 			}
 			else if (*format == 'c')
 			{
 				ch = va_arg(arg_list, int);
 				_putchar(ch, 1);
 				len++;
+				break;
 			}
 			else if (*format == 's')
 			{
 				str = va_arg(arg_list, char*);
 				print_string(str);
+				break;
 			}
 			format++;
 		}
 		_putchar(*(format), 1);
-		++format;
+		format++;
 	}
 
 	va_end(arg_list);

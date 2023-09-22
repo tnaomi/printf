3238fee1fd97601cafb859d1e109cf7d4415e0bc
KARENKAMORI <kamorikaren2019@gmail.com>
2023-09-22 08:20:14 -0700

    printf

---
+++ test/misc.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * _strlen - prints length of a string
+ * @str: input string
+ * Return: length
+*/
+int _strlen(char *str)
+{
+        unsigned int len = 0, indx = 0;
+
+        if (*str)
+        {
+                for (; str[indx] != '\0'; indx++)
+                        len++;
+        }
+        return (len);
+}
+
+/**
+ * _putchar - print single character
+ * @c: character
+ * @size: buffer size or length
+ * Return: character
+*/
+
+int _putchar(char c, unsigned int size)
+{
+        return (write(1, &c, size));
+}
+
+/**
+ * print_string - print a string from va_list
+ * @arg_list: Input string
+ * @len: length of character string
+* Return: len
+*/
+int print_string(va_list arg_list, unsigned int len)
+{
+        char *str = va_arg(arg_list, char *);
+
+        if (str == NULL)
+                str = "(NULL)";
+        len = _strlen(str);
+        while (*str != '\0')
+        {
+                _putchar(*str, 1);
+                len++;
+                str++;
+        }
+        return (len);
+}

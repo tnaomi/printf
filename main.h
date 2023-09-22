b1af1e5c425d51c64cc3ca333536bb54b2f71907
KARENKAMORI <kamorikaren2019@gmail.com>
2023-09-22 05:37:28 -0700

    main.h

--- main.h
+++ main.h
@@ -1,54 +1,119 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stdio.h>
 #include <stdarg.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <limits.h>
 
-#define TOTAL_HEX 16
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
 
 /* FLAGS */
-/* FLAGS */
-#define flag_plus 2
-#define flag_minus 1
-#define flag_hashtag 8
-#define flag_zero 4
-#define flag_space 16
+#define FLAG_MINUS 1
+#define FLAG_PLUS 2
+#define FLAG_ZERO 4
+#define FLAG_HASH 8
+#define FLAG_SPACE 16
+
+/* SIZES */
+#define STR_LONG 2
+#define STR_SHORT 1
+
+/* DEFINING A STRUCTURE */
+/**
+ * struct fmt - Struct type format
+ *
+ * @fmt: format type.
+ * @fn: Associated function.
+ */
+struct fmt
+{
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
+};
+
 
 /**
- * struct arguments - a defined structure of arguments
+ * typedef struct fmt fmt_t - Structure type
  *
- * @flag_plus: set when flag_plus is selected
- * @flag_minus: set when flag_minus is selected
- * @flag_zero: set when flag_zero is selected
- * @flag_hashtag: set when flag_hashtag is selected
- * @flag_space: set when flag_space is selected
+ * @fmt: format type.
+ * @fm_t: Associated function.
  */
-typedef struct arguments
-{
-	unsigned int flag_plus	: 1;
-	unsigned int flag_minus	: 1;
-	unsigned int flag_zero	: 1;
-	unsigned int flag_hashtag: 1;
-	unsigned int flag_space	: 1;
-} parameter;
+typedef struct fmt fmt_t;
 
-int _printf(const char *format, ...);
-int select_f(const char *format, va_list arg_list, unsigned int len);
-int binary(unsigned int num, unsigned int len);
-int address(va_list arg_list, unsigned int len);
-int octal(int num);
+int _printf(const char *format, ...); /* printf function */
+int print_handling(const char *fmt, int *ind, va_list list, char buff[],
+	int flag, int width, int prec, int size);
 
-/*HELPER FUNCTIONS*/
-int _strlen(char *str);
-int _putchar(char c, unsigned int size);
-int print_string(va_list arg_list, unsigned int len);
-int print_integer(va_list arg_list, unsigned int len);
+/* FUNCTIONS */
 
-/* FLAG FUNCTIONS */
-int set_flags(const char *format, int *i);
-int _flag(char *str, parameter *parameters);
-void _parameters(parameter *parameters, va_list n);
+/* Funtions that print characters and strings */
+int percent_print(va_list type, char buffer[],
+	int flags, int width, int precision, int size);
+int string_print(va_list type, char buffer[],
+	int flags, int width, int precision, int size);
+int char_print(va_list type, char buffer[],
+	int flags, int width, int precision, int size);
+
+/* Function printing numbers */
+
+int binary_print(va_list type, char buffer[],
+	int flags, int width, int prec, int size);
+int hexadecimal_print(va_list type, char buff[],
+	int flag, int width, int prec, int size);
+int octal_print(va_list type, char buff[],
+	int flag, int width, int prec, int size);
+int unsigned_print(va_list type, char buff[],
+	int flag, int width, int prec, int size);
+int hexa_upper_print(va_list type, char buff[],
+	int flag, int width, int prec, int size);
+int int_print(va_list type, char buffer[],
+	int flags, int width, int precision, int size);
+
+int print_hexa(va_list type, char map_to[], char buff[],
+	int flag, char flag_char, int width, int prec, int size)
+
+/* Function to print non printable characters */
+int non_printable_print(va_list type, char buffer[],
+	int flags, int width, int precision, int size);
+
+/* Funcion to print memory address */
+int pointer_print(va_list type, char buff[],
+	int flag, int width, int prec, int size);
+
+/* Functions handling other specifiers */
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
+
+/*Function to print string in reverse*/
+int reverse_print(va_list type, char buffer[],
+	int flags, int width, int precision, int size);
+
+/*Function to print a string in rot 13*/
+int rot13string_print(va_list type, char buff[],
+	int flag, int width, int prec, int size);
+
+/* width handler */
+int handle_write_char(char c, char buff[],
+	int flag, int width, int prec, int size);
+int write_number(int is_negative, int ind, char buff[],
+	int flag, int width, int prec, int size);
+int write_num(int ind, char buff[], int flags, int width, int prec,
+	int len, char padd, char extra_c);
+int write_pointer(char buff[], int ind, int len,
+	int width, int flag, char padd, char extra_c, int padd_start);
+
+int write_unsgnd(int is_negative, int ind,
+char buff[],
+	int flag, int width, int prec, int size);
+
+/* UTILITIES */
+int is_printable(char);
+int append_hexa_code(char, char[], int);
+int is_digit(char);
+
+long int convert_size_number(long int num, int size);
+long int convert_size_unsgnd(unsigned long int num, int size);
 
 #endif /* MAIN_H */
---
+++ test/printf.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+* _printf - printf function
+* @format : format string
+* Return: returns length
+* (null terminating character isn't considered)
+*/
+
+int _printf(const char *format, ...)
+{
+        unsigned int len = 0; /*string_length = 0; tracks characters length*/
+        va_list arg_list;
+
+        if (format == NULL) /*Terminate the program if the format string is empty*/
+                return (-1);
+
+        va_start(arg_list, format); /*format string is last function argument*/
+
+        while (*format != '\0')
+        {
+                if (*format == '%')
+                {
+                        format++; /*skips the % character*/
+                        len = select_f(format, arg_list, len);
+                        format++;
+                }
+                else
+                {
+                        _putchar(*format, 1);
+                        len++;
+                        format++;
+                }
+        }
+        va_end(arg_list);
+	return (len);
+}
